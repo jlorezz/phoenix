4af1331dff18c0a01fd6049b518f407af4b76163
Jlorezz <florianbress@gmail.com>
2024-07-21 18:28:56 +0200

    Update hardware.nix

--- hosts/nas/hardware.nix
+++ hosts/nas/hardware.nix
@@ -9,7 +9,7 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "dm-raid" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
