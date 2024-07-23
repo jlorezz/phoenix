b38d0b9e23228658f4a9c2cf1298e236c30d4362
jlorezz <florianbress@gmail.com>
2024-07-23 13:03:56 +0200

    correct setup for nas

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -1,14 +1,14 @@
 { config, pkgs, lib, username, ... }:
 
 {
-  imports = [./hardware.nix];
+  imports = [ ./hardware.nix ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  
+  networking.hostName = "nixos";
+ 
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Berlin";
@@ -27,18 +27,23 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
 
+  # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -46,10 +51,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
 
   users.users.${username}.extraGroups = lib.mkAfter ["networkmanager"];
   
@@ -57,12 +65,14 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.05";  
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 }
