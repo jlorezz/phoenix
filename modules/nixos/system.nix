238580737c9f52ef92e34d2ee10be7d410fd3fee
jlorezz <florianbress@gmail.com>
2024-08-13 19:38:39 +0200

    adjustements to default.nix for desktop setup

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -82,6 +82,8 @@ with lib; {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
+
+    package = pkgs.nixVersions.latest;
   };
 
   documentation = mkDefault {
