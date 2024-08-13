ce966c83e70cdfb8f6ad8fc1179cd2a58acd421c
jlorezz <florianbress@gmail.com>
2024-08-13 16:30:10 +0000

    adjustements and adding desktop

--- modules/flake/nixosConfigurations.nix
+++ modules/flake/nixosConfigurations.nix
@@ -19,5 +19,6 @@
   in {
     wsl = systemConfig "x86_64-linux" [../../hosts/wsl.nix];
     nas = systemConfig "x86_64-linux" [../../hosts/nas];
+    desktop = systemConfig "x86_64-linux" [../hosts/desktop];
   };
 }
