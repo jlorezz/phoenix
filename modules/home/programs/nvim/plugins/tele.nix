c6288394b5ec6828ce9a7a5385d127cdcb505f38
jlorezz <florianbress@gmail.com>
2024-08-16 18:39:55 +0200

    adjustements to tele plugin for nvim

--- modules/home/programs/nvim/plugins/tele.nix
+++ modules/home/programs/nvim/plugins/tele.nix
@@ -1,9 +1,11 @@
+{ config, pkgs, ... }:
+
 {
   programs.nixvim.plugins.telescope = {
     enable = true;
-
+    
     enabledExtensions = [
-      "git_worktree"
+      "git_worktree" # Load the git_worktree extension
     ];
 
     settings = {
@@ -25,80 +27,112 @@
         };
         mappings = {
           i = {
-            ["<C-u>"] = false;
-            ["<C-d>"] = false;
-            ["<C-j>"] = {
-              __raw = "require('telescope.actions').move_selection_next";
-            };
-            ["<C-k>"] = {
-              __raw = "require('telescope.actions').move_selection_previous";
-            };
-            ["<C-d>"] = {
-              __raw = "require('telescope.actions').move_selection_previous";
-            };
+            "<C-u>" = false;
+            "<C-d>" = false;
+            "<C-j>" = "require('telescope.actions').move_selection_next";
+            "<C-k>" = "require('telescope.actions').move_selection_previous";
           };
         };
       };
     };
-
-  keymaps = {
-    normalMode = {
-      ["<leader>?"] = {
-        action = "builtin.oldfiles";
-        options = { desc = "[?] Find recently opened files"; };
+    keymaps = {
+      "<C-p>" = {
+        action = "builtin.git_files";
+        options = {
+          desc = "Telescope Git Files";
+        };
       };
-      ["<leader>/"] = {
-        __raw = "function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = true }) end";
-        options = { desc = "[/] Fuzzily search in current buffer]"; };
-      };
-      ["<leader>sf"] = {
-        action = "builtin.find_files";
-        options = { desc = "[S]earch [F]iles"; };
-      };
-      ["<leader>sw"] = {
-        action = "builtin.grep_string";
-        options = { desc = "[S]earch current [W]ord"; };
-      };
-      ["<leader>sg"] = {
+      "<leader>fg" = {
         action = "builtin.live_grep";
-        options = { desc = "[S]earch by [G]rep"; };
+        options = {
+          desc = "Telescope Live Grep";
+        };
       };
-      ["<leader>sd"] = {
+      "<leader>?" = {
+        action = "builtin.oldfiles";
+        options = {
+          desc = "[?] Find recently opened files";
+        };
+      };
+      "<leader>/" = {
+        action = "function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = true }) end";
+        options = {
+          desc = "[/] Fuzzily search in current buffer]";
+        };
+      };
+      "<leader>sf" = {
+        action = "builtin.find_files";
+        options = {
+          desc = "[S]earch [F]iles";
+        };
+      };
+      "<leader>sw" = {
+        action = "builtin.grep_string";
+        options = {
+          desc = "[S]earch current [W]ord";
+        };
+      };
+      "<leader>sg" = {
+        action = "builtin.live_grep";
+        options = {
+          desc = "[S]earch by [G]rep";
+        };
+      };
+      "<leader>sd" = {
         action = "builtin.diagnostics";
-        options = { desc = "[S]earch [D]iagnostics"; };
+        options = {
+          desc = "[S]earch [D]iagnostics";
+        };
       };
-      ["<leader>sb"] = {
+      "<leader>sb" = {
         action = "builtin.buffers";
-        options = { desc = "[ ] Find existing buffers"; };
+        options = {
+          desc = "[ ] Find existing buffers";
+        };
       };
-      ["<leader>sS"] = {
+      "<leader>sS" = {
         action = "builtin.git_status";
-        options = { desc = ""; };
+        options = {
+          desc = "";
+        };
       };
-      ["<leader>sm"] = {
-        __raw = ":Telescope harpoon marks<CR>";
-        options = { desc = "Harpoon [M]arks"; };
+      "<leader>sm" = {
+        action = ":Telescope harpoon marks<CR>";
+        options = {
+          desc = "Harpoon [M]arks";
+        };
       };
-      ["<Leader>sr"] = {
-        __raw = "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>";
-        options = { silent = true; };
+      "<Leader>sr" = {
+        action = "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>";
+        options = {
+          silent = true;
+        };
       };
-      ["<Leader>sR"] = {
-        __raw = "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>";
-        options = { silent = true; };
+      "<Leader>sR" = {
+        action = "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>";
+        options = {
+          silent = true;
+        };
       };
-      ["<Leader>sn"] = {
-        __raw = "<CMD>lua require('telescope').extensions.notify.notify()<CR>";
-        options = { silent = true; };
+      "<Leader>sn" = {
+        action = "<CMD>lua require('telescope').extensions.notify.notify()<CR>";
+        options = {
+          silent = true;
+        };
       };
-      ["st"] = {
-        __raw = ":TodoTelescope<CR>";
-        options = { noremap = true; };
+      "st" = {
+        action = ":TodoTelescope<CR>";
+        options = {
+          noremap = true;
+        };
       };
-      ["<Leader><tab>"] = {
-        __raw = "<Cmd>lua require('telescope.builtin').commands()<CR>";
-        options = { noremap = false; };
+      "<Leader><tab>" = {
+        action = "<Cmd>lua require('telescope.builtin').commands()<CR>";
+        options = {
+          noremap = false;
+        };
       };
     };
   };
 }
+
