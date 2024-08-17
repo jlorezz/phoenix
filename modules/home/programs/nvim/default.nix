d0d04bcec96c8edf384fa969327521cd05476371
jlorezz <florianbress@gmail.com>
2024-08-17 22:04:46 +0200

    adjustements to neovim plugins

--- modules/home/programs/nvim/default.nix
+++ modules/home/programs/nvim/default.nix
@@ -25,6 +25,37 @@
     viAlias = true;
     vimAlias = true;
 
-    luaLoader.enable = true;
+    plugins = {
+      gitsigns = {
+        enable = true;
+        settings.signs = {
+          add.text = "+";
+          change.text = "~";
+        };
+      };
+
+      nvim-autopairs.enable = true;
+
+      nvim-colorizer = {
+        enable = true;
+        userDefaultOptions.names = false;
+      };
+
+      oil.enable = true;
+
+      trim = {
+        enable = true;
+        settings = {
+          highlight = true;
+          ft_blocklist = [
+            "checkhealth"
+            "floaterm"
+            "lspinfo"
+            "neo-tree"
+            "TelescopePrompt"
+          ];
+        };
+      };
+    };
   };
 }
